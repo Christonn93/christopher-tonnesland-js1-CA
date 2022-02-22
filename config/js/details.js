b282d3d9bf87bc955e4f36dd2affb510ea65b3e6
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-02-22 19:17:41 +0100

    progressing

--- config/js/details.js
+++ config/js/details.js
@@ -6,42 +6,47 @@ const container = document.querySelector(".container");
 container.innerHTML = "";
 const footer = document.querySelector(".footer");
 
+// Setting new param
+const params = new URLSearchParams(window.location.href);
+
 // Api call
 const cors = "https://noroffcors.herokuapp.com/";
-const API_url = `https://openlibrary.org/subjects/computer_science.json`;
+const API_url = `https://openlibrary.org/` +params;
 
 const API = cors + API_url;
 
-// API call function
 async function API_call() {
   try {
-    //Declaring the call and response to the API
+
+    //Declaring the call and response to the API 
     const call = await fetch(API);
     const json = await call.json();
-    const bookArray = json.works;
+    const bookDetail = json.works;
 
-    for (let i = 0; i < bookArray.length; i++) {
-      // Declaring book information
-      const bookAuthor = bookArray[i].authors[0].name;
-      const bookTitle = bookArray[i].title;
-      const bookID = bookArray[i].key;
+    for (let i = 0; i < bookDetail.length; i++){
 
-      // API call for the book cover
-      const bookIsbn = bookArray[i].availability.isbn;
-      const size = `L`;
-      const bookImg = `https://covers.openlibrary.org/b/isbn/${bookIsbn}-${size}.jpg`;
+    // Declaring book information
+    const bookAuthor = bookDetail[i].authors[0].name;
+    const bookTitle = bookDetail[i].title;
+    const bookID = bookDetail[i].key;
 
-      // Populating the header
-      header.innerHTML = `<h1>${json.name}</h1>`;
+    // API call for the book cover
+    const bookIsbn = bookDetail[i].availability.isbn;
+    const size = `L`;
+    const noImageFound = 'https://via.placeholder.com/500x500?text=Image+not+found';
+    const bookImg = `https://covers.openlibrary.org/b/isbn/${bookIsbn}-${size}.jpg`  || noImageFound;
+    
+    // Populating the header
+    header.innerHTML = `<h1>${json.name}</h1>`;
 
-      // Populating the container with books
-      container.innerHTML += ``;
+    // Populating the container with books    
+    container.innerHTML += ``;
     }
 
     console.log(json.works);
+   
   } catch (error) {
-    // Catching error if it is any
-    console.log("error", error);
+    console.log(error);
   }
 }
 
