8c2b78f4f4156f0f0c334661dac7626f16092cd2
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-02-26 14:52:00 +0100

    Progressing
    Adding somne progress

--- config/js/contact.js
+++ config/js/contact.js
@@ -6,35 +6,74 @@ import { setFooter } from "./modules/footer.js";
 setHeader2();
 setFooter();
 
-// Setting opp const and targeting HTML ID's
-const contactInputs = {
-    name: document.querySelector("#name"),
-    subject: document.querySelector("#subject"),
-    email: document.querySelector("#email"),
-    adress: document.querySelector("#adress"),
-    button: document.querySelector("button")
-}
 
-// Adding Event listener
-contactInputs.addEventListener();
-
-const name = document.querySelector("#name");
-const subject = document.querySelector("#subject");
+// Selecting HTML elements
+const form = document.querySelector("#form");
+const popUp = document.querySelector(".pop-up")
+const fullName = document.querySelector("#fullName");
+const fullNameError = document.querySelector("#fullNameInfo");
 const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailInfo");
 const adress = document.querySelector("#adress");
-const button = document.querySelector("button");
+const adressError = document.querySelector("#adressInfo");
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectInfo");
+const submitButton = document.querySelector("#submit");
 
+form.addEventListener("submit", formValidation);
 
+function formValidation(event){
+    event.preventDefault();
 
-name.addEventListener("keyup", checkButton);
-subject.addEventListener("keyup", checkButton);
-adress.addEventListener("keyup", checkButton);
-email.addEventListener("keyup", checkButton);
-
-
-// Contact form function
-function contactForm(){
-
+    if(lengthValidation(fullName.value, 0) === true){
+        fullNameError.style.display = "none";
+    }else{
+        fullNameError.style.display = "inline-flex";
+    }
     
+    if(emailValidation(email.value) === true){
+        emailError.style.display = "none";
+    }else{
+        emailError.style.display = "inline-flex";
+    }
+
+    if(lengthValidation(adress.value, 24) === true){
+        adressError.style.display = "none";
+    }else{
+        adressError.style.display = "inline-flex";
+    }
+
+    if(lengthValidation(subject.value, 9) === true){
+        subjectError.style.display = "none";
+    }else{
+        subjectError.style.display = "inline-flex";
+    }
+
+
+    formSubmitted();
+
 };
-contactForm();
+
+function lengthValidation(value, len){
+    if (value.trim().length > len){
+        return true;
+    } else {
+        return false;
+    }
+};
+
+function emailValidation(email){
+    const regEx = /\S+@\S+\.\S+/;
+    const emailPattern = regEx.test(email);
+    return emailPattern;
+};
+
+function formSubmitted(event){
+    event.preventDefault();
+        popUp.style.display= "block";
+        popUp.innerHTML = `<h3>Thanks for contacting us</h3>
+        <p>We will get back to you shortly</p>`;
+    
+        form.reset();
+   
+};
